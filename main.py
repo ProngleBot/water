8a36a9640bbe8fa31957e6efe66a8940c668a53d
bigpapasg <shreeharibharadwaj0@gmail.com>
2020-09-06 15:59:59 +0530

    water reminder ready

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from discord_webhook import DiscordWebhook
 while True:
     for _ in range(6):
-        webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/741276735617761317/RDjGsSfa86vvOd8TqfRxKEUGTEpNRsTDTuizesoqJIY-FWOFB1VEwB9aMhIho_lP43OB', content='<@!518543956787593256> Drink water')
+        webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/752113109904392193/i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', content='<@!518543956787593256> Drink water')
         webhook.execute()
         print("shit worked")
     time.sleep(3600)
