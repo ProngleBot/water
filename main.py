24a65e90f9a712c699a1f99b7347b4e8860364d7
bigpapasg <shreeharibharadwaj0@gmail.com>
2020-09-17 11:26:23 +0530

    test

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from discord.ext import commands,tasks
 
 @tasks.loop(hours=1)
 async def waterrem():
-    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/752113109904392193/i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', content='<@&753266209633337405> Drink water')
+    webhookiboi = await fetch_webhook('i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof')
+    webhook = DiscordWebhook(url=f'https://discordapp.com/api/webhooks/752113109904392193/{webhookiboi}', content='<@&753266209633337405> drink water')
     webhook.execute()
     print("shit worked")
 
 @tasks.loop(hours=3)
-async def stretch():
-    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/752113109904392193/i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', content='<@&753266209633337405> Stand up and walk around!')
+async def stretch(webhook_id):
+    webhookiboi = await fetch_webhook('i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof')
+    webhook = DiscordWebhook(url=f'https://discordapp.com/api/webhooks/752113109904392193/{webhookiboi}', content='<@&753266209633337405> Stand up and walk around!')
     webhook.execute()
     print("shit worked")
 
