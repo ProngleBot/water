a4ba6a51aed6ecabf8fd30626cb8c312ce5f37d7
bigpapasg <shreeharibharadwaj0@gmail.com>
2020-09-17 11:09:10 +0530

    stretch added and made all these a loop

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import time
+import time,discord
 from discord_webhook import DiscordWebhook
-while True:
-    for _ in range(6):
-        webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/752113109904392193/i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', content='<@!518543956787593256> Drink water')
-        webhook.execute()
-        print("shit worked")
-    time.sleep(3600)
+from discord.ext import commands,tasks
+
+@tasks.loop(hours=1)
+async def waterrem():
+    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/752113109904392193/i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', content='<@&753266209633337405> Drink water')
+    webhook.execute()
+    print("shit worked")
+
+@tasks.loop(hours=3)
+async def stretch():
+    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/752113109904392193/i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', content='<@&753266209633337405> Stand up and walk around!')
+    webhook.execute()
+    print("shit worked")
+
+waterrem.start()
+stretch.start()
