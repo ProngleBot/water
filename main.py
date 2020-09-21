c37dea0da5d2edc8bfd009292b064495df9416ae
bigpapasg <40268788+bigpapasg@users.noreply.github.com>
2020-09-21 15:12:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-import requests,schedule,time
-from discord import Webhook, RequestsWebhookAdapter
-from discord.ext import tasks
-
-def water():
-    webhook = Webhook.partial(752113109904392193,'i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', adapter=RequestsWebhookAdapter())
-    webhook.send('<@&753266209633337405> Drink water ILY!!')
-    print('drank')
-def walk():
-    webhook = Webhook.partial(752113109904392193,'i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', adapter=RequestsWebhookAdapter())
-    webhook.send('<@&753266209633337405> Take a break and walk around :heart: :heart: ily!!')
-    print('walked')
-# schedule.every(1).hour.do(water)
-# schedule.every(6).hours.do(walk)
-schedule.every(1).hours.do(water)
-schedule.every(3).hours.do(walk)
+import time
+from discord_webhook import DiscordWebhook
 while True:
-    schedule.run_pending()
+    webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/752113109904392193/i3OXo7ct-D2tamzXgbGp6uzbMbN5aaYHYcagiAWeuuAZCrISo_qtyJKa8r0RmLd2fdof', content='<@&753266209633337405> Drink water and stay hydrated i love you :heart: :heart:')
+    webhook.execute()
+    print("shit worked")
+    time.sleep(3600)
